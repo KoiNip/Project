fb66677a2050415f7749270abca6e8da4dbe0c51
Relate <blismatic@gmail.com>
2022-05-03 13:48:00 -0700

    renamed main2 to main

--- main.py
+++ main.py
@@ -1,67 +1,35 @@
-import sys
 from Schedule import schedule
 import Task
+import consolemenu as cm
+import consolemenu.items as cmi
 
 def doSomeFunction():
-    print('function doSomeFunction() has ran')
+    print('doSomeFunction() executed...')
+
+def buildMenu(menu, directory):
+    for k, v in directory.items():
+        if type(v) == dict:
+            submenu = cm.ConsoleMenu(subtitle=k, clear_screen=False, exit_option_text='Return')
+            buildMenu(submenu, v)
+
+            menu.append_item(cmi.SubmenuItem(k, submenu, menu))
+        elif callable(v):
+            menu.append_item(cmi.FunctionItem(k, v))
+        else: raise AssertionError(f'This should never be executed.\nThe value associated with {k} was neither a dictionary or function')
+
+if __name__ == '__main__':
+    first_task = Task.Transient(
+        name='grocery shopping', 
+        category='Shopping', 
+        start_time=10.5, 
+        duration=2, 
+        date=20220514
+    )
     
-class TextGuidedMenuSystem:
-    def __init__(self, directory, decorator, indent, welcome_msg, main_msg):
-        self.directory = directory
-        self.decorator = decorator
-        self.indent = indent
-        self.welcome_msg = welcome_msg
-        self.main_msg = main_msg
-        
-    def run(self):
-        current_directory = self.directory
-        level = 0
-        
-        print(f'{self.welcome_msg}\n{self.decorator*50}')
-        while True:
-            spacing = self.indent*level
-            if level == 0: print(f'{self.main_msg}\n{self.decorator*len(self.main_msg)}')
-
-            # Display options from current directory
-            for idx, option in enumerate(current_directory):
-                print(f'{spacing}{idx} {option}')
-            print(f'q Exit') if level == 0 else print(f'{spacing}b Back')
-
-            # Get selection from user
-            selection = input(f'{spacing}> ')
-            print()
-
-            # Branch off based upon selection number
-            if selection in [str(i) for i, _ in enumerate(current_directory)]:
-                key = list(current_directory.keys())[int(selection)]
-                print(f'{spacing}{key}\n{spacing}{self.decorator*len(key)}')
-
-                # If the value of the selected key is a dictionary...
-                if type(current_directory[key]) == dict:
-                    current_directory = current_directory[key]
-                    level += 1
-
-                # If the value of the selected key is a function...
-                elif callable(current_directory[key]):
-                    current_directory[key]()
-                    continue
-
-                # If the value of the selected key is something else...
-                else: raise AssertionError(f'This should never be executed.\nThe value associated with {key = } was neither a dictionary or function')
-
-            elif selection == 'b' and level != 0:
-                level -= 1
-            elif selection == 'q' and level == 0:
-                print('Thanks for using PSS, goodbye.')
-                sys.exit(0)
-            else:
-                print(f'{spacing}Please select a valid option.')
-
-def main():
     directory = {
         "Tasks": {
             "Create": doSomeFunction,
-            "View": doSomeFunction,
+            "View": first_task.view,
             "Delete": doSomeFunction,
             "Edit": doSomeFunction
         },
@@ -81,15 +49,8 @@ def main():
         }
     }
     
-    main_menu = TextGuidedMenuSystem(
-        directory=directory,
-        decorator='-',
-        indent='    ',
-        welcome_msg='Welcome to PSS. What would you like to do?',
-        main_msg='Main menu'
-    )
+    schedule.add(first_task)
     
-    main_menu.run()
-
-if __name__ == '__main__':
-    main()
+    menu=cm.ConsoleMenu("PSS", "CS 3560", clear_screen=False)
+    buildMenu(menu, directory)
+    menu.show()
