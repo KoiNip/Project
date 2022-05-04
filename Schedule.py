aebbebad5d2ad1f4143b1ea975337a9f40c310f5
Jose Pavon <RannerJP@gmail.com>
2022-05-04 16:19:34 -0700

    Comments - better error handling

--- Schedule.py
+++ Schedule.py
@@ -1,4 +1,3 @@
-from pickle import FALSE, TRUE
 import Task
 from Date import Date
 import os
@@ -30,62 +29,111 @@ class Schedule:
             'Cancellation': 'Anti'
         }
         
-        names = [task.name for task in self._tasks]
-        task_name = input("Input a new task name:")
-        while task_name in names or task_name == "ALL" or task_name.isnumeric() == True:
-            if task_name in names:
+        names = [task.name for task in self._tasks] #Grabs all task names to 
+        
+        task_name = input("Input a new task name: ")
+        while task_name in names or task_name == "ALL" or task_name.isnumeric() == True: #Checks if the name is already used, if the name is "ALL" (See view function), or if the name is only numbers
+            if task_name in names: #Case if name has already been used
                 task_name = input(f"The task name {task_name} has already been used, please input another task name: ")
-            if task_name == "ALL":
+            if task_name == "ALL": #Case if name is "ALL"
                 task_name = input(f"The task name {task_name} can't be used, please input another task name: ")
-            if task_name.isnumeric() == True:
+            if task_name.isnumeric() == True: # Case if name is only numbers "ex: 1, 2332"
                 task_name = input(f"The task name can't be all numbers, please input another task name: ")
+        
         task_type = input("Input the task type: ")
-        while task_type not in task_types.keys():
+        while task_type not in task_types.keys(): #Checks to see if the task type inputted is valid
             task_type = input(f"{task_type} is not a vaild task type. Please input a valid task type: ")
-        task_category = task_types[task_type]
-        task_date = int(input("Input the (start) date of the task"))
-        valid_date = FALSE
-        while (valid_date == FALSE):
+        task_category = task_types[task_type] #Saves what kind of task the task type is for later use
+        task_date = input("Input the (start) date of the task: ")
+        
+        valid_date = False
+        while (valid_date == False): #Trys to create a date object since that validates the input in it's initalization
             try:
                 test_date = Date(task_date)
-            except AssertionError:
-                task_date = int(input(f"{task_date} is not a valid date, input a valid date: "))
+            except AssertionError: #Date was not in a valid format
+                task_date = input(f"{task_date} is not a valid date, input a valid date: ")
+            else: #Date is in a valid format and we can continue along
+                valid_date = True
+        valid_date = False #Resets to False in case we have a Recurring Task
+        
+        time_validation = True
+        try: #This try catch makes sure the user inputted a float
+            task_time = float(input("Please input the start time from 0.00 to 24.00: "))
+        except:
+            print("That was an invalid input type, setting time to -1: ")
+            task_time = -1
+            time_validation = False
+        while 0 > task_time or task_time > 24 or time_validation==False: 
+            try: #In case the user enters a string or char
+                task_time = float(input(f"{task_time} is not a valid start time, please input a valid start time: "))
+            except:
+                print("That was an invalid input type")
+                time_validation = False
+                task_time = -1
             else:
-                valid_date = TRUE
-        valid_date = FALSE
-        task_time = float(input("Please input the start time to the nearest quarter (:15 = .25, :30 = .5, :45 = .75, :00 = .00):"))
-        while 0 > task_time or task_time > 24 or task_time % .25 != 0: 
-            task_time = float(input(f"{task_time} is not a valid start time, please input a valid start time"))
-        task_duration = float(input("Please input the duration to the nearest quarter (:15 = .25, :30 = .5, :45 = .75, :00 = .00)"))
-        while task_duration > 23.75 or task_duration < .25 or task_duration % .25 != 0:
-            task_duration = float(input(f"{task_duration} is not a valid duration. Please input a rounded number between .25 and 23.75: "))
-        if task_category == "Recurring":
-            end_date= int(input("Input the end date of the task: "))
-            while (valid_date == FALSE):
+                time_validation = True
+       
+        duration_validation = True
+        try: #makes sure the duration is a valid float
+             task_duration = float(input("Please input a duration between 0.25 and 23.75: "))
+        except:
+            print("That was an invalid input, setting duration to -1: ")
+            task_duration = -1
+            duration_validation = False
+        while task_duration > 23.75 or task_duration < .25 or duration_validation==False: 
+            try: #In case the user enters a string or char
+                task_duration = float(input(f"{task_duration} is not a valid duration. Please input a number between .25 and 23.75: "))
+            except:
+                print("That was an invalid input type")
+                duration_validation = False
+                task_duration = -1
+            else:
+                duration_validation = True
+        
+        if task_category == "Recurring": #We got the task type earlier so now we create the correct one, in this case a Recurring task
+            end_date= input("Input the end date of the task: ") #Recurring needs two more piece of info: end date and frequency
+            while (valid_date == False): #Validates the date
                 try:
                     test_date = Date(end_date)
                 except AssertionError:
-                    task_date = int(input(f"{end_date} is not a valid date, input a valid date: "))
+                    end_date = input(f"{end_date} is not a valid date, input a valid date: ")
                 else:
-                    valid_date = TRUE
-            frequency = int(input("Input how frequent the task should occur (1 == daily, 7== weekly): "))
-            while frequency > 7 and frequency < 1:
-                frequency = int(input(f"{frequency} is not a vaild frequency, please input a number from 1 to 7"))
+                    valid_date = True
+            
+            frequency_validation = True
+            try: #Makes sure the frequency is valid
+                frequency = int(input("Input how frequent the task should occur (1 == daily, 7== weekly): "))
+            except:
+                print("That is not a valid integer! setting frequency to -1")
+                frequency = -1
+                frequency_validation = False
+            while (frequency != 1 and frequency != 7) or frequency_validation==False: #Also makes sure the user input a 1 or a 7
+                try: #In case the user enters a string or char
+                    frequency = int(input(f"{frequency} is not a valid frequency. Please input either 1 or 7: "))
+                except:
+                    print("That was an invalid input")
+                    frequency_validation = False
+                    frequency = -1
+                else:
+                    frequency_validation = True
+            
             #add overlap here when function finished
             recurring_name = Task.Recurring(task_name, task_type, task_time, task_duration, task_date, end_date, frequency)
-            self._tasks.append(recurring_name)
+            self._tasks.append(recurring_name) #Adds the task to a list of all task objects
             print(f"{task_name} has been added!")
             return
+        
         elif task_category == "Anti":
             #add overlap here when function finished
             anti_name = Task.Anti(task_name, task_type, task_time, task_duration, task_date)
-            self._tasks.append(anti_name)
+            self._tasks.append(anti_name) #Adds the task to a list of all task objects
             print(f"{task_name} has been added!")
             return
-        else:
+       
+        else: #Task would have to be a Transient task
             #add overlap here when function is finished
             transient_name = Task.Transient(task_name, task_type, task_time, task_duration, task_date)
-            self._tasks.append(transient_name)
+            self._tasks.append(transient_name) #Adds the task to a list of all task objects
             print(f"{task_name} has been added!")
             return
     
