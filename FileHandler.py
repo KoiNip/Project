40130f68df8dd38a56c0204ae48782684a2c3df7
Makarius Salib <misalib@cpp.edu>
2022-05-06 18:19:36 -0700

    Defines functions for writing only specific schedule dates
    Makes the list of tasks a member instead of a class variable
    Imports the Date class for the FileHandler to use

--- FileHandler.py
+++ FileHandler.py
@@ -1,9 +1,10 @@
+from calendar import week
 import json
 import os
 from os.path import exists
-from Schedule import schedule
+from Schedule import Schedule, schedule
 import Task
-
+from Date import Date
 class FileHandler():
     def read(self):
         file_name = input("File name to read from? >> ")
@@ -30,7 +31,7 @@ class FileHandler():
         else:
             print(f'Schedule loaded from \'{file_path}\' successfully!')
     
-    def write(self, file_name=None, overwrite=False):
+    def write(self, sched = schedule, file_name=None, overwrite=False):
         if file_name == None:
             file_name = input("File name to write to? >> ")
         if not file_name.endswith('.json'): file_name += '.json'
@@ -40,7 +41,7 @@ class FileHandler():
             if exists(file_path) and not overwrite: raise FileExistsError
             
             with open(file_path, 'w') as f:
-                json.dump(schedule._tasks, f, indent=4, default=Task.Task.serialize)
+                json.dump(sched._tasks, f, indent=4, default=Task.Task.serialize)
         
         except FileExistsError:
             print(f'File \'{file_path}\' already exists.')
@@ -57,14 +58,31 @@ class FileHandler():
         else:
             print(f'Schedule written to \'{file_path}\' successfully!')
             
-    def write_day(self):
-        pass
+    def write_day(self, date):
+        daySchedule = Schedule()
+        allDayTask = Task.Transient("ALL_DAY", Task.Transient.task_types[0], 0, 23.75, date)
+        for task in schedule._tasks:
+            if task.overlaps(allDayTask):
+                daySchedule._tasks.append(task)
+        self.write(daySchedule)
     
-    def write_week(self):
-        pass
+    def write_week(self, date):
+        weekSchedule = Schedule()
+        longTask = Task.Recurring("ALL_WEEK", Task.Recurring.task_types[0], 0, 23.75, date, date.plus(7), 1)
+        for task in schedule._tasks:
+            if task.overlaps(longTask):
+                weekSchedule._tasks.append(task)
+        self.write(weekSchedule)
     
-    def write_month():
-        pass
+    def write_month(self, month, year):
+        monthSchedule = Schedule()
+        start = Date(year + month + "01")
+        end = Date(year + month + Date._calendar[month]['days'])
+        longTask = Task.Recurring("ALL_MONTH", Task.Recurring.task_types[0], 0, 23.75, )
+        for task in schedule._tasks:
+            if task.overlaps(longTask):
+                monthSchedule._tasks.append(task)
+        self.write(week)
 
 # -----------------------------------
 file_handler = FileHandler()
